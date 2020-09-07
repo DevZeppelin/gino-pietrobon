b944b458bc04de678714f165e4e3611ac2f8a391
Gino <gini77oo@gmail.com>
2020-09-07 15:00:40 -0300

    Updates

---
+++ precache-manifest.d19c06ef340297f9377b0a46e85f9da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0810b4eb8c3574504dc68e99d9848e17",
+    "url": "/gino-pietrobon/index.html"
+  },
+  {
+    "revision": "ad29cb29db0029c80225",
+    "url": "/gino-pietrobon/static/css/main.9ef645fd.chunk.css"
+  },
+  {
+    "revision": "fbce0f7a3b992c71d786",
+    "url": "/gino-pietrobon/static/js/2.30d88bed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gino-pietrobon/static/js/2.30d88bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad29cb29db0029c80225",
+    "url": "/gino-pietrobon/static/js/main.cba35076.chunk.js"
+  },
+  {
+    "revision": "66455bc3065604854285",
+    "url": "/gino-pietrobon/static/js/runtime-main.a195cf73.js"
+  },
+  {
+    "revision": "e07a51bab572fe64e3cb0464255b7d47",
+    "url": "/gino-pietrobon/static/media/fondo.e07a51ba.jpg"
+  }
+]);
