69f806b279b65f0113fcf7429c06f7d349d03dc9
Gino <gini77oo@gmail.com>
2020-09-07 18:19:28 -0300

    changes on blog.js

--- src/blog.js
+++ src/blog.js
@@ -7,15 +7,15 @@ const BlogList = [
     {
         id: '1',
         img: 'imagenes/blog1.png',
-        title: 'Las empresas necesitan una página web que hable por ellas',
-        p: 'Este es el ejemplo de una empresa tecnológica que me confió el desarrollo de su sitio web. ¿Querés ver como quedó?',
+        title: 'El poder de una página web',
+        p: 'Toda empresa de hoy tiene su propia página web. Algunas se ven antiguas, otras mucho más modernas. Detrás de cada web está la tecnología con la que fueron construídas. La mayoría de las webs por mucho tiempo fueron construídas con Wordpress, pero hoy existen tecnologías muchísimo más potentes, rápidas, brindando una experiencia al usuario inigualable.',
         link: 'http://devzeppelin.github.io/powersis'
     },
     {
         id: '2',
         img: 'imagenes/blog2.jpg',
-        title: '¿Como sería una tarjeta de invitación estilo sitio web?',
-        p: 'Una idea original y revolucionaria. Toda la información y genial diseño. Un gran evento merece una genial invitación.',
+        title: 'Sabiendo lo básico de programación podemos crear algo único ',
+        p: 'Una vez me llegó una invitación a un casamiento, era una invitación muy original, increíble y con mucha información. Para poner en práctica mis estudios me puse a hacer una invitación similar pero programada por mí. Me gustó como quedó y entendí que con poco código y buenas ideas, podemos hacer grandes cosas. Mirá como quedó mi página:',
         link: 'http://devzeppelin.github.io/InvitacionWeb'
     }
 
